b8dc32ef38f2bf98998fc344e106a111660e69d5
Developer-Jaber <jaberhossain.web.developer@gmail.com>
2025-09-21 15:08:17 +0400

    feat:Project Structure Organize into Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { ContentModule } from './content/content.module';
+import { ProgressModule } from './progress/progress.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, SubscriptionsModule, ContentModule, ProgressModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
